d0c35fdf601169ebbbcb1a54f4961c75049c492e
Hannah Mumma <hannahmumma@gmail.com>
2020-02-27 10:25:51 -0500

    clean up spacing and add comments

--- weather-coding-exercise/part-2/scripts/weather.js
+++ weather-coding-exercise/part-2/scripts/weather.js
@@ -1,12 +1,14 @@
 /**
  * Weather Code Challenge
  *
- * 
+ * This code challenge uses a jsonp callback function to display data from an external weather data source.
  *
  */
 
-const hdnWeatherJsonpCallback = (data) => {
+// This is our jsonp callback function. We'll call our populateCityData here so that we can display
+// our content
 
+const hdnWeatherJsonpCallback = data => {
   const body = document.getElementsByTagName("body")[0],
     cities = data.cities,
     citySelect = document.getElementById("city-select"),
@@ -16,101 +18,85 @@ const hdnWeatherJsonpCallback = (data) => {
     currentCond = current.querySelector(".current-cond"),
     forecast = document.getElementById("forecast"),
     weekly = forecast.querySelector(".weekly-forecast");
-  
+
   for (let city of cities) {
-
-    console.log(city)
-
     const selectOption = document.createElement("option");
 
     selectOption.innerHTML = city.geoloc.city;
     citySelect.appendChild(selectOption);
 
     if (city.geoloc.city === "Palo Alto") {
-
       selectOption.setAttribute("selected", true);
       populateCityData(subheading, currentTemp, currentCond, city, weekly);
-
     }
 
     citySelect.addEventListener("change", function() {
-      
-      // document.querySelector("option[selected=true]").removeAttribute("selected");
 
       if (citySelect.value === city.geoloc.city) {
-        
         selectOption.setAttribute("selected", true);
 
-        if (weekly.innerHTML !== ""){
+        if (weekly.innerHTML !== "") {
           weekly.innerHTML = "";
         }
 
         populateCityData(subheading, currentTemp, currentCond, city, weekly);
       }
-
     });
-
-  } 
-  
+  }
 };
 
+// This function will grab the jsonp data. We need this in two locations so we'll create a function for it.
+
 const populateCityData = (subheading, temp, condition, city, week) => {
+  subheading.innerHTML = `${city.geoloc.city} - Current Conditions: `;
 
-  subheading.innerHTML = city.geoloc.city;  
-
-  temp.innerHTML = `Current Temperature: ${city.current[0].temp} &#176;`;
-  condition.innerHTML = `Current Condition: ${city.current[0].condition}`;
+  temp.innerHTML = `${city.current[0].temp}&#176;`;
+  condition.innerHTML = `${city.current[0].condition}`;
 
   for (let day of city.weekly) {
-
     let weeklyListItem = document.createElement("li"),
-
       dayOfTheWeekContainer = document.createElement("div"),
       dayOfTheWeekTitle = document.createElement("h4"),
-
       high = document.createElement("h5"),
       low = document.createElement("h5"),
       dayOfTheWeekConditions = document.createElement("p");
 
     dayOfTheWeekContainer.className = "day";
-
     dayOfTheWeekTitle.innerHTML = `${day.day} ${day.date}`;
-
-    high.innerHTML = `${day.high} &#176;`;
-    low.innerHTML = `${day.low} &#176;`;
-
+    high.innerHTML = `${day.high}&#176;`;
+    low.innerHTML = `${day.low}&#176;`;
     dayOfTheWeekConditions.innerHTML = `${day.daycondition}`;
 
     dayOfTheWeekContainer.appendChild(dayOfTheWeekTitle);
     dayOfTheWeekContainer.appendChild(high);
     dayOfTheWeekContainer.appendChild(low);
-
     dayOfTheWeekContainer.appendChild(dayOfTheWeekConditions);
 
-
     weeklyListItem.appendChild(dayOfTheWeekContainer);
-    week.appendChild(weeklyListItem);            
-
+    week.appendChild(weeklyListItem);
   }
+};
 
-}
+// Here we'll create a script tag for the jsonp source so we can use the callback function to access the data
+// We're also adding our Google font to the head element.
 
 const addScriptsToHead = () => {
-  const head = document.getElementsByTagName("head")[0],  
-    script = document.createElement('script'),
-    url = "https://www.sfchronicle.com/external/weather/weather.json?callback=hdnWeatherJsonpCallback";
+  const head = document.getElementsByTagName("head")[0],
+    script = document.createElement("script"),
+    url =
+      "https://www.sfchronicle.com/external/weather/weather.json?callback=hdnWeatherJsonpCallback";
 
   sourceSansProFont = document.createElement("link");
-  sourceSansProFont.href = "https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap";
+  sourceSansProFont.href =
+    "https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap";
   sourceSansProFont.rel = "stylesheet";
-  
   script.src = url;
 
   head.appendChild(sourceSansProFont);
   head.appendChild(script);
-
 };
 
+// Initialize
 
 const init = () => {
   addScriptsToHead();
