8b99649c3b0beb9443c95f9ef768eaf8ace18660
Hannah Mumma <hannahmumma@gmail.com>
2020-02-28 13:15:35 -0500

    add darkmode functionality

--- weather-coding-exercise/part-2/scripts/weather.js
+++ weather-coding-exercise/part-2/scripts/weather.js
@@ -9,6 +9,7 @@
 // source's cities object.
 
 const hdnWeatherJsonpCallback = data => {
+
   const body = document.getElementsByTagName("body")[0],
     cities = data.cities,
     citySelect = document.getElementById("city-select"),
@@ -17,10 +18,12 @@ const hdnWeatherJsonpCallback = data => {
     currentTemp = current.querySelector(".current-temp"),
     currentCond = current.querySelector(".current-cond"),
     forecast = document.getElementById("forecast"),
-    weekly = forecast.querySelector(".weekly-forecast");
+    weeklyForecast = forecast.querySelector(".weekly-forecast");
+
+  // Get the hour of the day so we can check if we need to add darkmode
+  getHourOfDay(body);  
 
   // loop through our object to get data for each city
-
   for (let city of cities) {
     const selectOption = document.createElement("option");
 
@@ -29,22 +32,20 @@ const hdnWeatherJsonpCallback = data => {
 
     if (city.geoloc.city === "Palo Alto") {
       selectOption.setAttribute("selected", true);
-      populateCityData(subheading, currentTemp, currentCond, city, weekly);
+      populateCityData(subheading, currentTemp, currentCond, city, weeklyForecast);
     }
 
    // We'll need to check if a user selects a new city so we can add an event listener for that 
-
     citySelect.addEventListener("change", function() {
       if (citySelect.value === city.geoloc.city) {
         selectOption.setAttribute("selected", true);
 
         // Check if the data is already populated. If it is, empty it out so we can populate another city's data if a user 
         // selects another city
-
-        if (weekly.innerHTML !== "") {
-          weekly.innerHTML = "";
+        if (weeklyForecast.innerHTML !== "") {
+          weeklyForecast.innerHTML = "";
         }
-        populateCityData(subheading, currentTemp, currentCond, city, weekly);
+        populateCityData(subheading, currentTemp, currentCond, city, weeklyForecast);
       }
     });
   }
@@ -55,34 +56,45 @@ const hdnWeatherJsonpCallback = data => {
 const populateCityData = (subheading, temp, condition, city, week) => {
 
   // This takes care of the selected city's current conditions
-  subheading.innerHTML = `${city.geoloc.city} - Current Conditions: `;
+  subheading.innerHTML = `${city.geoloc.city}`;
   temp.innerHTML = `${city.current[0].temp}&#176;`;
   condition.innerHTML = `${city.current[0].condition}`;
 
-  // Here we'll retrieve a city's weekly information from our cities object
-
+  // Here we'll retrieve a city's weekly information from the cities object
   for (let day of city.weekly) {
     let weeklyListItem = document.createElement("li"),
       dayOfTheWeekContainer = document.createElement("div"),
       dayOfTheWeekTitle = document.createElement("h4"),
-      high = document.createElement("h5"),
-      low = document.createElement("h5"),
+      lowHigh = document.createElement("h5"),
       dayOfTheWeekConditions = document.createElement("p");
 
     dayOfTheWeekContainer.className = "day";
     dayOfTheWeekTitle.innerHTML = `${day.day} ${day.date}`;
-    high.innerHTML = `${day.high}&#176;`;
-    low.innerHTML = `${day.low}&#176;`;
-    dayOfTheWeekConditions.innerHTML = `${day.daycondition}`;
+    lowHigh.innerHTML = `${day.low}&#176;/${day.high}&#176;`;
+
+    // Handle our daymode/darkmode needs
+    const darkmode = document.querySelectorAll(".darkmode");
+    let condition = darkmode.length === 0 ? day.daycondition : day.nightcondition;
+    dayOfTheWeekConditions.innerHTML = condition;
 
     dayOfTheWeekContainer.appendChild(dayOfTheWeekTitle);
-    dayOfTheWeekContainer.appendChild(high);
-    dayOfTheWeekContainer.appendChild(low);
+    dayOfTheWeekContainer.appendChild(lowHigh);
     dayOfTheWeekContainer.appendChild(dayOfTheWeekConditions);
 
     weeklyListItem.appendChild(dayOfTheWeekContainer);
     week.appendChild(weeklyListItem);
   }
+};
+
+// Get the hour of the day so we can setup styling on the class darkmode 
+const getHourOfDay = (body) => {
+  const date = new Date(),
+    hours = date.getHours();
+  
+  if (hours >= 20 || hours < 7) {
+    body.classList.add("darkmode");
+  }
+  return hours;
 };
 
 // Here we'll create a script tag for the jsonp source so we can use the callback function to access the data
